5bcaa8c953c021f88eae524a6c2100e26affa0f6
Aleksei Zolotykh <aleksei.zolotykh@syngenta.com>
2022-04-22 10:49:57 +0200

    Fixes

--- test/apigateway/endpoint-config/file-config-reader.test.js
+++ test/apigateway/endpoint-config/file-config-reader.test.js
@@ -4,21 +4,21 @@ const { assert } = require('chai');
 
 describe('FileConfigReader', () => {
     it('should read config file from folder without index.js', () => {
-        const modulePath = 'test/apigateway/mocks/enpoint-config/random-dir-name';
+        const modulePath = 'test/apigateway/endpoint-config/mocks/random-dir-name';
         const module = require('./mocks/random-dir-name');
         const fileConfigReader = new FileConfigReader({modulePath});
         assert.deepEqual(fileConfigReader.read(), module);
     });
 
     it('should read config file from folder with index.js', () => {
-        const modulePath = path.join('random-dir-name', 'index.js');
+        const modulePath = 'test/apigateway/endpoint-config/mocks/random-dir-name/index.js';
         const module = require('./mocks/random-dir-name');
-        const fileConfigReader = new FileConfigReader({modulePath, requirer});
-        assert.deepEqual(fileConfigReader.read(), module);
+        const fileConfigReader = new FileConfigReader({modulePath});
+        assert.equal(fileConfigReader.read(), module.get);
     });
 
     it('should fail if path not found', () => {
         const modulePath = path.join('random', 'module', 'name')
-        assert.throws(() => new FileConfigReader({modulePath, requirer}), 'can\'t resolve module random/module/name');
+        assert.throws(() => new FileConfigReader({modulePath}), 'can\'t resolve module random/module/name');
     });
 });
